56e9978453120d27ad2a7264dabfea78a4ce4ddc
Marcelo Souto <marcelosouto676@gmail.com>
2022-10-29 10:38:38 -0300

    teste

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 import jsonServer from 'json-server'
 const server = jsonServer.create();
-const routes = jsonServer.routes('routes.json')
 const router = jsonServer.router('db.json'); // <== Will be created later
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200; // <== You can change the port
 
-server.use(routes)
 server.use(middlewares);
 server.use(router);
 
